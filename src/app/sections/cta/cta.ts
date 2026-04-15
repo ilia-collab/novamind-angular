874cb854f17ea9408633528454527b8fa36c2f96
ilia-collab <iliamagaria90@gmail.com>
2026-04-15 22:01:55 +0400

    Animations left

--- src/app/sections/cta/cta.ts
+++ src/app/sections/cta/cta.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './cta.html',
   styleUrl: './cta.css',
 })
-export class Cta {}
+export class Cta {
+  title = "Ready to unlock your"
+  titleColoring = "team's potential?"
+  desc = "Join 50,000+ teams already using NovaMind to ship faster, collaborate smarter, and stay ahead."
+  buttonLabel = "Get Started — It's Free"
+}
+ 
